577403f2af66f555b1281f2f3816e8bbc77f2d59
kehan <kehan@fanmiot.cn>
2023-04-06 17:22:05 +0800

    build: 添加sequelize 依赖

--- config/config.default.js
+++ config/config.default.js
@@ -1,30 +1,39 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+'use strict'
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {};
+module.exports = (appInfo) => {
+	/**
+	 * built-in config
+	 * @type {Egg.EggAppConfig}
+	 **/
+	const config = (exports = {})
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1680751916447_5415';
+	// use for cookie sign key, should change to your own and keep security
+	config.keys = appInfo.name + '_1680751916447_5415'
 
-  // add your middleware config here
-  config.middleware = [];
+	// add your middleware config here
+	config.middleware = []
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
+	config.sequelize = {
+		dialect: 'mysql',
+		host: 'localhost',
+		port: 3306,
+		database: 'egg-sequelize-default',
+		username: 'root',
+		password: '',
+	}
 
-  return {
-    ...config,
-    ...userConfig,
-  };
-};
+	// add your user config here
+	const userConfig = {
+		// myAppName: 'egg',
+	}
+
+	return {
+		...config,
+		...userConfig,
+	}
+}
--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+    // had enabled by egg
+    // static: {
+    //   enable: true,
+    // }
+
+    sequelize: {
+        enable: true,
+        package: "egg-sequelize",
+    },
 };
